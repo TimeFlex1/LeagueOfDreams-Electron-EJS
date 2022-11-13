4cef25598005f7c9e4f9abfa3ee4d7c572adeff2
Tintarul <appledeathXNX@gmail.com>
2022-11-13 14:27:10 +0200

    Update secret.js

--- config/secret.js
+++ config/secret.js
@@ -1,5 +1,5 @@
 module.exports = {
-	database: 'mongodb://localhost:27017/databaseName',
+	database: 'mongodb://127.0.0.1:27017/databaseName',
 	port: 3000,
 	secretKey: "RECOMMENDED TO USE A RANDOM KEY AT LEAST 32 CHARACTERS LONG",	
 }
